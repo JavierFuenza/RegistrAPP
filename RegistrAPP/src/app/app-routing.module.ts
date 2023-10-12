930ca5a3e69ba192b153368f0fafd558219880ba
JavierFuenza <106996489+JavierFuenza@users.noreply.github.com>
2023-10-12 14:50:35 -0300

    cambio login page

--- RegistrAPP/src/app/app-routing.module.ts
+++ RegistrAPP/src/app/app-routing.module.ts
@@ -1,30 +1,23 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
-  /*
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  */
   {
     path: '',
     loadChildren: () => import('./registro/login/login.module').then( m => m.LoginPageModule)
   },
+  /*
   {
     path: 'register',
     loadChildren: () => import('./registro/register/register.module').then( m => m.RegisterPageModule)
   },
+  */
   {
     path: 'recuperar',
     loadChildren: () => import('./registro/recuperar/recuperar.module').then( m => m.RecuperarPageModule)
-  },  {
+  },
+  {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
