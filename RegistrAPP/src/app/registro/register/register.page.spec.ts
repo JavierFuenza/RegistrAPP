50c77a43b709870597e6218df2ccd302e18391fd
Franclas <129339415+Franclas@users.noreply.github.com>
2023-10-19 21:30:50 -0300

    cc

--- RegistrAPP/src/app/registro/register/register.page.spec.ts
+++ RegistrAPP/src/app/registro/register/register.page.spec.ts
@@ -15,3 +15,7 @@ describe('RegisterPage', () => {
     expect(component).toBeTruthy();
   });
 });
+function async(arg0: () => void): jasmine.ImplementationCallback {
+  throw new Error('Function not implemented.');
+}
+
