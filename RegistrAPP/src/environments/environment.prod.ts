1048ee311751a51f8eca4c702875cf277f3b807c
Franclas <129339415+Franclas@users.noreply.github.com>
2023-11-23 02:33:30 -0300

    firebase

--- RegistrAPP/src/environments/environment.prod.ts
+++ RegistrAPP/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDTyH5I2S6tkoh8el7z0DDrvs1klBC15zk",
+    authDomain: "registrapp-9212d.firebaseapp.com",
+    databaseURL: "https://registrapp-9212d-default-rtdb.firebaseio.com",
+    projectId: "registrapp-9212d",
+    storageBucket: "registrapp-9212d.appspot.com",
+    messagingSenderId: "546342050673",
+    appId: "1:546342050673:web:07d1e86103915edccaf4d0",
+    measurementId: "G-XJ14PW157K"
+  }
 };
--- RegistrAPP/src/environments/environment.ts
+++ RegistrAPP/src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDTyH5I2S6tkoh8el7z0DDrvs1klBC15zk",
+    authDomain: "registrapp-9212d.firebaseapp.com",
+    databaseURL: "https://registrapp-9212d-default-rtdb.firebaseio.com",
+    projectId: "registrapp-9212d",
+    storageBucket: "registrapp-9212d.appspot.com",
+    messagingSenderId: "546342050673",
+    appId: "1:546342050673:web:07d1e86103915edccaf4d0",
+    measurementId: "G-XJ14PW157K"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
