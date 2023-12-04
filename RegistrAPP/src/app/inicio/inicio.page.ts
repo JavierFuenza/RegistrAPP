369b22ef057d19303d59ce9264ba886fa3d545de
JavierFuenza <ja.fuenzalida@duocuc.cl>
2023-12-04 17:31:04 -0300

    1

--- RegistrAPP/src/app/inicio/inicio.page.ts
+++ RegistrAPP/src/app/inicio/inicio.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseAuthenticationServiceService } from '../shared/firebase-authentication-service.service';
-
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -9,10 +10,38 @@ import { FirebaseAuthenticationServiceService } from '../shared/firebase-authent
 })
 export class InicioPage implements OnInit {
 
-  constructor( public authService: FirebaseAuthenticationServiceService) { }
+  isSupported = false;
+  barcodes: Barcode[] = [];
+
+  constructor( public authService: FirebaseAuthenticationServiceService, private alertController: AlertController) { }
   ngOnInit(){
-    //console.log(this.authService.verStorage())
-  };
-  
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
+  }
+
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permiso denegado',
+      message: 'Debes dar permisos para acceder a la camara',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 
 }
